ea87ac64dbc9c71869ad3956dde9c572485a1f12
Jason Etheridge <jason@esilibrary.com>
2015-02-27 01:32:28 -0500

    instructions for Windows, and tweak to run-win.bat
    Signed-off-by: Jason Etheridge <jason@esilibrary.com>

--- run-win.bat
+++ run-win.bat
@@ -1,5 +1,5 @@
-SET JAVA_HOME="C:\Program Files\Java\jdk1.8.0_20"
-SET JETTY_HOME=jetty-distribution-9.1.4.v20140401
+SET JAVA_HOME="C:\Program Files\Java\jdk1.8.0_31"
+SET JETTY_HOME=jetty
 
 %JAVA_HOME%\bin\javac -cp "%JETTY_HOME%\lib\*;%JETTY_HOME%\lib\websocket\*;lib\*" -Xdiags:verbose -d lib src\org\evergreen_ils\hatch\*.java
 
